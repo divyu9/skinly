5df23899bd983a2dba38a28f7c98f74dd7d4067b
Chandan <94528601+divyu9@users.noreply.github.com>
2025-12-25 10:32:08 +0530

    use-auth.ts

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -1,64 +1,12 @@
-import { useCallback, useEffect, useMemo } from "react";
-import { useAuth as useOidcAuth } from "react-oidc-context";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
-type UseAuthHook = {
-  fetchAccessToken: (args: {
-    forceRefreshToken: boolean;
-  }) => Promise<string | null>;
-} & ReturnType<typeof useOidcAuth>;
-
-export function useAuth(): UseAuthHook {
-  const oidcAuth = useOidcAuth();
-
-  const idToken = oidcAuth.user?.id_token;
-  const fetchAccessToken = useCallback(
-    // eslint-disable-next-line no-empty-pattern
-    async ({}: { forceRefreshToken: boolean }) => {
-      // TODO: refresh token if needed
-      return idToken ?? null;
-    },
-    [idToken],
-  );
-
-  return useMemo(
-    () => ({
-      ...oidcAuth,
-      fetchAccessToken,
-    }),
-    [oidcAuth, fetchAccessToken],
-  );
-}
-
-type UseUserProps = {
-  /**
-   * Whether to automatically redirect to the login if the user is not authenticated
-   */
-  shouldRedirect?: boolean;
-};
-
-export function useUser({ shouldRedirect }: UseUserProps = {}) {
-  const { user, isLoading, error, isAuthenticated, signinRedirect } = useAuth();
-
-  useEffect(() => {
-    if (!isLoading && !isAuthenticated && shouldRedirect) {
-      signinRedirect();
-    }
-  }, [isLoading, isAuthenticated, shouldRedirect, signinRedirect]);
-
-  return useMemo(() => {
-    const id = user?.profile.sub;
-    const name = user?.profile.name;
-    const email = user?.profile.email;
-    const avatar = user?.profile.picture;
-    return {
-      ...(user ?? {}),
-      id,
-      name,
-      email,
-      avatar,
-      isAuthenticated,
-      isLoading,
-      error,
-    };
-  }, [user, isAuthenticated, isLoading, error]);
+export function useAuth() {
+  const user = useQuery(api.users.currentUser);
+  
+  return {
+    user: user,
+    isLoading: user === undefined,
+    isAuthenticated: !!user,
+  };
 }
